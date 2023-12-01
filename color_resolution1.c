3fce8c807359ba5627a4d3fd335079a88c4e82de
Ved1073 <152605586+Ved1073@users.noreply.github.com>
2023-12-01 16:42:22 +0530

    Add files via upload

---
+++ color_resolution1.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include<pthread.h>
+// Video resolution
+#define W 420
+#define H 880
+
+// Allocate a buffer to store one frame
+unsigned char frame[H][W][3] = {0};
+void* colorinvert(){
+   int x, y, count;
+
+    // Open an input pipe from ffmpeg and an output pipe to a second instance of ffmpeg
+    FILE *pipein = popen("ffmpeg -i output.mp4 -f image2pipe -vcodec rawvideo -pix_fmt rgb24 -", "r");
+    FILE *pipeout = popen("ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x720 -r 25 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 coloredOutput1.mp4", "w");
+    FILE *pipeout1 = popen("ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x720 -r 25 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 coloredOutput1.txt", "w");
+
+    // Process video frames
+    while(1)
+    {
+        // Read a frame from the input pipe into the buffer
+        count = fread(frame, 1, H*W*3, pipein);
+
+        // If we didn't get a frame of video, we're probably at the end
+        if (count != H*W*3) break;
+
+        // Process this frame
+        for (y=0 ; y<H ; ++y) for (x=0 ; x<W ; ++x)
+        {
+            // Invert each colour component in every pixel
+            frame[y][x][0] = 255 - frame[y][x][0]; // red
+            frame[y][x][1] = 255 - frame[y][x][1]; // green
+            frame[y][x][2] = 255 - frame[y][x][2]; // blue
+        }
+
+        // Write this frame to the output pipe
+        fwrite(frame, 1, H*W*3, pipeout);
+        fwrite(frame, 1, H*W*3, pipeout1);
+    }
+
+    // Flush and close input and output pipes
+    fflush(pipein);
+    pclose(pipein);
+    fflush(pipeout);
+    pclose(pipeout);
+    fflush(pipeout1);
+    pclose(pipeout1);
+   }
+
+//for image resolution
+void* resolution(){
+   int x, y, count;
+   int Height,Width;
+    printf("enter the detailes to change the resoultion i.e height and width\n");
+    scanf("%d %d",&Height,&Width);
+    // Open an input pipe from ffmpeg and an output pipe to a second instance of ffmpeg
+    FILE *pipein = popen("ffmpeg -i coloredOutput1.mp4 -f image2pipe -vcodec rawvideo -pix_fmt rgb24 -", "r");
+    FILE *pipeout = popen("ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x720 -r 25 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 ResolutionOutput1.mp4", "w");
+    FILE *pipeout1 = popen("ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x720 -r 25 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 ResolutionOutput1.txt", "w");
+
+
+    // Process video frames
+    while(1)
+    {
+        // Read a frame from the input pipe into the buffer
+        count = fread(frame, 1, Height*Width*3, pipein);
+
+        // If we didn't get a frame of video, we're probably at the end
+        if (count != Height*Width*3) break;
+        // Write this frame to the output pipe
+        fwrite(frame, 1, Height*Width*3, pipeout);
+        fwrite(frame, 1, Height*Width*3, pipeout1);
+    }
+
+    // Flush and close input and output pipes
+    fflush(pipein);
+    pclose(pipein);
+    fflush(pipeout);
+    pclose(pipeout);
+    fflush(pipeout1);
+    pclose(pipeout1);
+   }
+//Image Stabilization
+void* image_stabilization(){
+      printf("Image stabilized\n");
+}
+int main()
+{
+  pthread_t thread_id1,thread_id2,thread_id3;
+  pthread_create(&thread_id1,NULL,colorinvert,NULL);
+  pthread_join(thread_id1,NULL);
+  pthread_create(&thread_id2,NULL,resolution,NULL);
+  pthread_join(thread_id2,NULL);
+  pthread_create(&thread_id3,NULL,image_stabilization,NULL);
+  pthread_join(thread_id3,NULL);
+    return 0;
+}
---
+++ color_resolution3.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include<pthread.h>
+#include<sys/types.h>
+#include<semaphore.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+// Video resolution
+#define W 420
+#define H 880
+sem_t semaphore;
+// Allocate a buffer to store one frame
+unsigned char frame[H][W][3] = {0};
+void* colorinvert(){
+   int x, y, count;
+    sem_wait(&semaphore);
+    // Open an input pipe from ffmpeg and an output pipe to a second instance of ffmpeg
+    FILE *pipein = popen("ffmpeg -i output3.mp4 -f image2pipe -vcodec rawvideo -pix_fmt rgb24 -", "r");
+    FILE *pipeout = popen("ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x720 -r 25 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 coloredOutput3.mp4", "w");
+    FILE *pipeout1 = popen("ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x720 -r 25 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 coloredOutput3.txt", "w");
+
+    // Process video frames
+    while(1)
+    {
+        // Read a frame from the input pipe into the buffer
+        count = fread(frame, 1, H*W*3, pipein);
+
+        // If we didn't get a frame of video, we're probably at the end
+        if (count != H*W*3) break;
+
+        // Process this frame
+        for (y=0 ; y<H ; ++y) for (x=0 ; x<W ; ++x)
+        {
+            // Invert each colour component in every pixel
+            frame[y][x][0] = 255 - frame[y][x][0]; // red
+            frame[y][x][1] = 255 - frame[y][x][1]; // green
+            frame[y][x][2] = 255 - frame[y][x][2]; // blue
+        }
+
+        // Write this frame to the output pipe
+        fwrite(frame, 1, H*W*3, pipeout);
+        fwrite(frame, 1, H*W*3, pipeout1);
+    }
+     sem_post(&semaphore);
+    // Flush and close input and output pipes
+    fflush(pipein);
+    pclose(pipein);
+    fflush(pipeout);
+    pclose(pipeout);
+    fflush(pipeout1);
+    pclose(pipeout1);
+    system("mplayer coloredOutput3.mp4");
+   }
+
+//for image resolution
+void* resolution(){
+   int x, y, count;
+   int Height,Width;
+   sem_wait(&semaphore);
+    printf("enter the detailes to change the resoultion i.e height and width\n");
+    scanf("%d %d",&Height,&Width);
+    // Open an input pipe from ffmpeg and an output pipe to a second instance of ffmpeg
+    FILE *pipein = popen("ffmpeg -i coloredOutput3.mp4 -f image2pipe -vcodec rawvideo -pix_fmt rgb24 -", "r");
+    FILE *pipeout = popen("ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x720 -r 25 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 ResolutionOutput3.mp4", "w");
+    FILE *pipeout1 = popen("ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x720 -r 25 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 ResolutionOutput3.txt", "w");
+
+
+    // Process video frames
+    while(1)
+    {
+        // Read a frame from the input pipe into the buffer
+        count = fread(frame, 1, Height*Width*3, pipein);
+
+        // If we didn't get a frame of video, we're probably at the end
+        if (count != Height*Width*3) break;
+        // Write this frame to the output pipe
+        fwrite(frame, 1, Height*Width*3, pipeout);
+        fwrite(frame, 1, Height*Width*3, pipeout1);
+    }
+    sem_post(&semaphore);
+    // Flush and close input and output pipes
+    fflush(pipein);
+    pclose(pipein);
+    fflush(pipeout);
+    pclose(pipeout);
+    fflush(pipeout1);
+    pclose(pipeout1);
+    system("mplayer ResolutionOutput3.mp4");
+   }
+//Image Stabilization
+void* image_stabilization(){
+      printf("Image stabilized\n");
+}
+int main()
+{
+   
+   sem_init(&semaphore,0,1);
+  pthread_t thread_id1,thread_id2,thread_id3;
+  pthread_create(&thread_id1,NULL,colorinvert,NULL);
+  pthread_join(thread_id1,NULL);
+  pthread_create(&thread_id2,NULL,resolution,NULL);
+  pthread_join(thread_id2,NULL);
+  pthread_create(&thread_id3,NULL,image_stabilization,NULL);
+  pthread_join(thread_id3,NULL);
+    return 0;
+}
